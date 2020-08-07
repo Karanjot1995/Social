a18f12297935bfef437ad41deaa54e05a17d462e
Karanjot Singh <karan.nanda97@gmail.com>
2020-08-07 19:51:59 +0530

    deployment and setup

--- gulpfile.js
+++ gulpfile.js
@@ -45,62 +45,30 @@ gulp.task('js', function () {
       merge: true
     }))
     .pipe(gulp.dest('./public/assets'));
+
 });
 // once uglified then revised the name then put it into public/assets then put details into manifest
 
 
-gulp.task('images', function () {
-  console.log('compressing images...');
-  return gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
-    .pipe(imagemin())
-    .pipe(rev())
-    .pipe(gulp.dest('./public/assets'))
-    .pipe(rev.manifest({
-      cwd: 'public',
-      merge: true
-    }))
-    .pipe(gulp.dest('./public/assets'));
+// gulp.task('images', function () {
+//   console.log('compressing images...');
+//   return gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
+//     .pipe(imagemin())
+//     .pipe(rev())
+//     .pipe(gulp.dest('./public/assets'))
+//     .pipe(rev.manifest({
+//       cwd: 'public',
+//       merge: true
+//     }))
+//     .pipe(gulp.dest('./public/assets'));
+// });
+
+gulp.task('clean:assets', function (done) {
+  del.sync('./public/assets');
+  done()
 });
 
-
-
-
-// empty the public/assets directory
-// gulp.task('clean-js', function () {
-//   return del([
-//     './public/js/*.js'
-//   ]);
-// });
-
-// gulp.task('clean-css', function () {
-//   return del([
-//     './public/css/*.css'
-//   ]);
-// });
-
-// gulp.task('clean-images', function () {
-//   return del([
-//     './public/images/*.+(png|jpg|gif|svg|jpeg)'
-//   ]);
-// });
-
-// gulp.task('js', ['clean-js'], function () {
-//   console.log('Building assets');
-//   return
-// })
-// gulp.task('css', ['clean-css'], function () {
-//   console.log('Building assets');
-//   return
-// })
-// gulp.task('images', ['clean-images'], function () {
-//   console.log('Building assets');
-//   return
-// })
-gulp.task('clean:assets', function () {
-  return del(['./public/assets'])
-});
-
-gulp.task('build', gulp.series('clean:assets', 'css', 'images', 'js'), function () {
+gulp.task('build', gulp.series('clean:assets', 'css', 'js'), function () {
   console.log('Building assets');
   return
 });
