ffe36910a6c732bb8e65ca769bb3d69ae5d88dc3
Karanjot Singh <karan.nanda97@gmail.com>
2020-08-09 02:27:24 +0530

    socket

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -19,8 +19,6 @@ class ChatEngine{
 
         this.socket.on('connect', function(){
             console.log('connection established using sockets...!');
-
-
             self.socket.emit('join_room', {
                 user_email: self.userEmail,
                 user_name: self.userName,
@@ -28,9 +26,8 @@ class ChatEngine{
             });
 
             self.socket.on('user_joined', function(data){
-                console.log('a user joined!', data);
+                console.log('new user joined!', data);
             })
-
 
         });
 
